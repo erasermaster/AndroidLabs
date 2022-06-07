2fce91b39df8a850891025354a6b716453367dcb
Xinyi Pan <pan00062@algonquinlive.com>
2022-06-07 01:15:15 -0400

    step1

--- app/src/main/java/com/cst2335/pan/MainActivity.java
+++ app/src/main/java/com/cst2335/pan/MainActivity.java
@@ -2,7 +2,14 @@ package com.cst2335.pan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btn = findViewById(R.id.buttonClickHere);
+        btn.setOnClickListener(
+                (v) -> Toast.makeText(getApplicationContext(), getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show());
+
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch swc = findViewById(R.id.switchOnOff);
+        swc = findViewById(R.id.switchOnOff);
+        String swcOn = getString(R.string.SwitchOn);
+        String swcOff = getString(R.string.SwitchOff);
+        String undo = getString(R.string.Undo);
+
+        Switch finalSwc = swc;
+        swc.setOnCheckedChangeListener((buttonView, isChecked) ->{
+            finalSwc.setText(isChecked?swcOn:swcOff);
+            Snackbar.make(btn,(isChecked?swcOn:swcOff),Snackbar.LENGTH_LONG).setAction( undo, click -> finalSwc.setChecked(!isChecked)).show();
+
+        });
+
+
     }
 }
+
+
